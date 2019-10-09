299050dd6b40c411b4e9138e926ae5cdbd662c40
FernandoGM2701 <fernandogm2701@gmail.com>
2019-10-09 11:28:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/generarimei/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.generarimei;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
